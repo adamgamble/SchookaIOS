4dadb4f79993acbc9c646baf3c74b5b24bb67b4b
Adam Gamble <adamgamble@Adam-Gambles-MacBook-Pro.local>
2011-07-18 11:21:00 -0500

    Initial Commit

---
+++ SchookaBudget/SchookaBudgetAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SchookaBudgetAppDelegate.h
+//  SchookaBudget
+//
+//  Created by Adam Gamble on 7/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SchookaBudgetAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
